c2e83528df1791408c9160851f2b2f4f2addc45f
hmdrz <hmdrzsharifi@gmail.com>
2024-02-19 00:52:12 +0330

    update python

--- chart-backend/rest/eodhd-resource.py
+++ chart-backend/rest/eodhd-resource.py
@@ -1,8 +1,9 @@
-from flask import Flask, Blueprint, request
+from flask import Flask, Blueprint, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+import requests
 
-from eodhd import APIClient
+# from eodhd import APIClient
 
 pd.set_option('display.float_format', '{:.8f}'.format)
 
@@ -18,12 +19,18 @@ def fetch_candle_data():
     from_time = request_data.get('from')
     to_time = request_data.get('to')
 
-    response_data = api.get_historical_data(symbol, tf, from_time, to_time)
-    response = response_data.to_json(orient='records', lines=True).splitlines()
+    # response_data = api.get_historical_data(symbol, tf, from_time, to_time)
+    # response = response_data.to_json(orient='records', lines=True).splitlines()
+    url = f"https://eodhd.com/api/eod/{symbol}?from={from_time}&to={to_time}&period={tf}&api_token={SECRET}&fmt=json"
 
-    return response
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify(response.text)
 
 
 if __name__ == '__main__':
-    api = APIClient("62c547eb00d445.30059582")
+    SECRET = "62c547eb00d445.30059582"
+    # api = APIClient("62c547eb00d445.30059582")
     app.run(host='0.0.0.0', port=5000, debug=True)
--- chart-backend/websocket-ticker/eodhistoricaldata-websocket-ticker.py
+++ chart-backend/websocket-ticker/eodhistoricaldata-websocket-ticker.py
@@ -1,27 +1,46 @@
 import time
 import pandas as pd
 from eodhd import WebSocketClient
+import psycopg2
 
 pd.set_option('display.float_format', '{:.8f}'.format)
 
 client = WebSocketClient(
-     api_key="62c547eb00d445.30059582",
-     endpoint="crypto",
-     symbols=["BTC-USD"],
-     display_stream=True,
-     display_candle_1m=False,
-     display_candle_5m=False,
-     display_candle_1h=False,
- )
- client.start()
+    api_key="62c547eb00d445.30059582",
+    endpoint="crypto",
+    symbols=["BTC-USD"],
+    display_stream=True,
+    display_candle_1m=False,
+    display_candle_5m=False,
+    display_candle_1h=False,
+)
+client.start()
 
- try:
-     msg = None
-     while client.running:
-         if msg is not None and client.message != msg:
-             print(client.message)
-             // todo insert into timescaledb
-         msg = client.message
- except KeyboardInterrupt:
-     print("\nStopping due to user request.")
-     client.stop()
+
+CONNECTION = "postgres://username:password@host:port/dbname"
+conn = psycopg2.connect(CONNECTION)
+cursor = conn.cursor()
+# use the cursor to interact with your database
+cursor.execute("SELECT 'hello world'")
+print(cursor.fetchone())
+
+sensors = [('a', 'floor'), ('a', 'ceiling'), ('b', 'floor'), ('b', 'ceiling')]
+cursor = conn.cursor()
+for sensor in sensors:
+  try:
+    cursor.execute("INSERT INTO sensors (type, location) VALUES (%s, %s);",
+                (sensor[0], sensor[1]))
+  except (Exception, psycopg2.Error) as error:
+    print(error.pgerror)
+conn.commit()
+
+
+try:
+    msg = None
+    while client.running:
+        if msg is not None and client.message != msg:
+            print(client.message)
+        msg = client.message
+except KeyboardInterrupt:
+    print("\nStopping due to user request.")
+    client.stop()
