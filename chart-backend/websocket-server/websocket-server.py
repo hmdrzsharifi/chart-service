33268227b3b98f81019f413876f9db7b22a92c91
h_sharifi <ujmUJM@1234567>
2024-02-19 18:53:59 +0330

    add timescaledb

--- chart-backend/websocket-server/websocket-server.py
+++ chart-backend/websocket-server/websocket-server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit, send
-
+import psycopg2
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -23,13 +23,26 @@ def handle_disconnect():
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data)
+    handle_candle_message(data)
 
 
 def handle_candle_message(message):
-    print("Handling message in main:", message)
-    socketio.send({'server_message': message})
-    socketio.emit({'message': message})
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT * FROM chart WHERE symbol = ? AND date = ?",
+                       (message[0], message[1]))
+        data = cursor.fetchall()
+        socketio.send({'server_message': data})
+        socketio.emit({'message': data})
+    except (Exception, psycopg2.Error) as error:
+        print(error.pgerror)
+    conn.close()
+
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8004, debug=False)
+    CONNECTION = "postgres://postgres:postgres@adi.dev.modernisc.com:5432/chart"
+conn = psycopg2.connect(CONNECTION)
+cursor = conn.cursor()
+
+socketio.run(app, host='0.0.0.0', port=8004, debug=False)
--- chart-backend/websocket-ticker/eodhistoricaldata-websocket-ticker.py
+++ chart-backend/websocket-ticker/eodhistoricaldata-websocket-ticker.py
@@ -9,36 +9,30 @@ client = WebSocketClient(
     api_key="62c547eb00d445.30059582",
     endpoint="crypto",
     symbols=["BTC-USD"],
-    display_stream=True,
-    display_candle_1m=False,
+    display_stream=False,
+    display_candle_1m=True,
     display_candle_5m=False,
     display_candle_1h=False,
 )
 client.start()
 
-
-CONNECTION = "postgres://username:password@host:port/dbname"
+CONNECTION = "postgres://postgres:postgres@adi.dev.modernisc.com:5432/chart"
 conn = psycopg2.connect(CONNECTION)
 cursor = conn.cursor()
-# use the cursor to interact with your database
-cursor.execute("SELECT 'hello world'")
-print(cursor.fetchone())
-
-sensors = [('a', 'floor'), ('a', 'ceiling'), ('b', 'floor'), ('b', 'ceiling')]
-cursor = conn.cursor()
-for sensor in sensors:
-  try:
-    cursor.execute("INSERT INTO sensors (type, location) VALUES (%s, %s);",
-                (sensor[0], sensor[1]))
-  except (Exception, psycopg2.Error) as error:
-    print(error.pgerror)
-conn.commit()
-
 
 try:
     msg = None
     while client.running:
         if msg is not None and client.message != msg:
+            candle_1m = json.loads(client.message)
+
+            cursor = conn.cursor()
+            try:
+                cursor.execute("INSERT INTO stock_prices_1m (symbol, date, open, high, low, close, volume) VALUES (%s, %s, %s, %s, %s, %s, %s);",
+                               (candle_1m[0], candle_1m[1], candle_1m[2], candle_1m[3], candle_1m[4], candle_1m[5], candle_1m[6]))
+            except (Exception, psycopg2.Error) as error:
+                print(error.pgerror)
+            conn.commit()
             print(client.message)
         msg = client.message
 except KeyboardInterrupt:
