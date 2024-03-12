42cd91a258f2bb1e5f991103118d85955d10ad03
h_sharifi <ujmUJM@1234567>
2024-03-12 08:43:11 +0330

    rename durationData to timeFrame

--- chart-frontend/src/util/store.ts
+++ chart-frontend/src/util/store.ts
@@ -1,29 +1,36 @@
 import { create } from 'zustand';
 
 type Store = {
-    durationData: 'd' | '1m' | 'SECONDS'
+    timeFrame: 'd' | '1m' | 'SECONDS'
     themeMode: 'dark' | 'light';
     setThemeMode: (mode: 'dark' | 'light') => void
-    setDurationData: (duration: 'd' | '1m' | 'SECONDS') => void
+    setTimeFrame: (duration: 'd' | '1m' | 'SECONDS') => void
     symbol: string
     setSymbol: (symbol: string) => void
     enableTrendLine: boolean
     setEnableTrendLine: (enableTrendLine: boolean) => void
     enableFib: boolean
     setEnableFib: (enableFib: boolean) => void
+
+    loadingMoreData: boolean
+    setLoadingMoreData: (loadingMoreData: boolean) => void
+
 };
 
 const useStore = create<Store>((set) => ({
     themeMode: 'light',
     setThemeMode: (mode: 'dark' | 'light') => set((state) => ({ themeMode: mode })),
-    durationData: 'd',
-    setDurationData: (duration: 'd' | '1m' | 'SECONDS') => set((state) => ({ durationData: duration })),
+    timeFrame: 'd',
+    setTimeFrame: (duration: 'd' | '1m' | 'SECONDS') => set((state) => ({ timeFrame: duration })),
     symbol: 'BTC-USD.CC',
     setSymbol: (symbol: string) => set((state) => ({ symbol: symbol })),
     enableTrendLine: false,
     setEnableTrendLine: (enableTrendLine: boolean) => set((state) => ({ enableTrendLine: enableTrendLine })),
     enableFib: false,
-    setEnableFib: (enableFib: boolean) => set((state) => ({ enableFib: enableFib }))
+    setEnableFib: (enableFib: boolean) => set((state) => ({ enableFib: enableFib })),
+
+    loadingMoreData: false,
+    setLoadingMoreData: (loadingMoreData: boolean) => set((state) => ({ loadingMoreData: loadingMoreData }))
 }));
 
 export default useStore;
