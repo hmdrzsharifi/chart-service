4aa7a4263810842e7819b4d75e267f0d60a437e3
h_sharifi <ujmUJM@1234567>
2024-03-12 08:45:47 +0330

    rename timeFrame

--- chart-frontend/src/App.tsx
+++ chart-frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
  const [data, setData] = useState<any>([]);
 
-    const { themeMode, symbol, durationData } = useStore();
+    const {themeMode, symbol, timeFrame} = useStore();
     // const [socket, setSocket] = useState(new WebSocket(WEBSOCKET_ADDRESS))
     const [socket, setSocket] = useState();
 
@@ -28,7 +28,7 @@ function App() {
         // Set up event handlers
         newSocket.on('connect', () => {
             console.log('Connected to server');
-            const msg= {
+            const msg = {
                 symbol: symbol,
                 timeFrame: '5s'
             }
@@ -39,7 +39,7 @@ function App() {
             console.log('Disconnected from server');
         });
 
-        newSocket.on('message', (data:any) => {
+        newSocket.on('message', (data: any) => {
             console.log('Received message:', data);
             // handleNewTick(data)
             handleRealTimeTick(data)
@@ -53,7 +53,7 @@ function App() {
     }, [symbol]);
 
 
-    const handleRealTimeTick = (message:any) => {
+    const handleRealTimeTick = (message: any) => {
         let originalData = JSON.parse(message.server_message);
         let date = new Date(originalData.t);
         let newCandle = {
@@ -69,12 +69,12 @@ function App() {
             "percentChange": ""
         };
 
-        setData((data: any[]) => [...data.slice(0, data.length-1), newCandle])
+        setData((data: any[]) => [...data.slice(0, data.length - 1), newCandle])
     }
 
     function generateFakeData() {
         setInterval(() => {
-            setData((data: any[]) => [...data.slice(0, data.length-1),
+            setData((data: any[]) => [...data.slice(0, data.length - 1),
                 {
                     "date": new Date("1970-01-20T15:15:50.400Z"),
                     "open": Math.random() * 170,
@@ -88,7 +88,7 @@ function App() {
                     // "close": 177.49,
                     // "volume": 57266675,
 
-            }])
+                }])
         }, 1000);
     }
 
@@ -97,9 +97,9 @@ function App() {
         fetchInitialData();
 
         // generateFakeData();
-    }, [symbol, durationData]); // Only on mount and unmount
+    }, [symbol, timeFrame]); // Only on mount and unmount
 
-    /*useEffect(() => {
+/*    useEffect(() => {
         // if (ticker === 'BINANCE:BTCUSDT' || ticker === 'BINANCE:ETHUSDT') {
         const socket = new WebSocket('wss://ws.finnhub.io?token=cneoim9r01qq13fns8b0cneoim9r01qq13fns8bg');
 
@@ -122,97 +122,97 @@ function App() {
     }, []);*/
 
 
-      /*const connectWebSocket = async () => {
-        /!*  ws.current = new WebSocket(WS_URL);
-          ws.current.onopen = () => {
-              console.log("WebSocket connected");
-              ws.current.send("subscribe:" + symbol);
-          };
-          ws.current.onmessage = (event) => {
-              handleNewTick(event.data);
-          };
-          ws.current.onerror = (error) => {
-              console.error('WebSocket Error:', error);
-          };
-          ws.current.onclose = () => {
-              console.log('WebSocket Disconnected');
-          };*!/
+    /*const connectWebSocket = async () => {
+      /!*  ws.current = new WebSocket(WS_URL);
+        ws.current.onopen = () => {
+            console.log("WebSocket connected");
+            ws.current.send("subscribe:" + symbol);
+        };
+        ws.current.onmessage = (event) => {
+            handleNewTick(event.data);
+        };
+        ws.current.onerror = (error) => {
+            console.error('WebSocket Error:', error);
+        };
+        ws.current.onclose = () => {
+            console.log('WebSocket Disconnected');
+        };*!/
 
-        // const socket = new WebSocket(WEBSOCKET_ADDRESS);
-        socket.onopen = () => {
-          console.log('WebSocket connection opened');
-          // socket.send("subscribe:" + symbol);
-        };
-        socket.onmessage = (event) => {
-          handleNewTick(event.data);
-        };
-        socket.onerror = (error) => {
-          console.error('WebSocket Error:', error);
-        };
-        socket.onerror = () => {
-          console.log('WebSocket Disconnected');
-        };
+      // const socket = new WebSocket(WEBSOCKET_ADDRESS);
+      socket.onopen = () => {
+        console.log('WebSocket connection opened');
+        // socket.send("subscribe:" + symbol);
       };
+      socket.onmessage = (event) => {
+        handleNewTick(event.data);
+      };
+      socket.onerror = (error) => {
+        console.error('WebSocket Error:', error);
+      };
+      socket.onerror = () => {
+        console.log('WebSocket Disconnected');
+      };
+    };
 */
-      const handleNewTick = (message:any) => {
+    const handleNewTick = (message: any) => {
         /*const cleanedMessage = message.replace(/\[|\]/g, '');
         const lastHyphenIndex = cleanedMessage.lastIndexOf("-");
         const ltp = parseFloat(cleanedMessage.substring(lastHyphenIndex + 1));
         lastTickPrice.current = ltp;*/
 
-        setData((prevData:any) => {
-          if (!prevData || prevData.length === 0) return null;
-          const newData = [...prevData];
-          const lastCandle = {...newData[newData.length - 1]};
-          const currentTime = new Date();
+        setData((prevData: any) => {
+            if (!prevData || prevData.length === 0) return null;
+            const newData = [...prevData];
+            const lastCandle = {...newData[newData.length - 1]};
+            const currentTime = new Date();
 
-          // Calculate the start time for the next candle based on the last candle's date
-          // const timeFrameMinutes = parseInt(timeFrame.replace('minute', ''), 10);
-          const nextCandleStartTime = new Date(lastCandle.date);
-          // nextCandleStartTime.setMinutes(nextCandleStartTime.getMinutes() + timeFrameMinutes);
+            // Calculate the start time for the next candle based on the last candle's date
+            // const timeFrameMinutes = parseInt(timeFrame.replace('minute', ''), 10);
+            const nextCandleStartTime = new Date(lastCandle.date);
+            // nextCandleStartTime.setMinutes(nextCandleStartTime.getMinutes() + timeFrameMinutes);
 
-          if (currentTime >= nextCandleStartTime) {
-            // Time to start a new candle
-            console.log("appending new candle");
-            console.log("appending new candle", message);
-            // let originalData = JSON.parse(message);
-            let originalData = JSON.parse(message.server_message);
-            let date = new Date(originalData.t);
-            let newCandle = {
-              "date": date,
-              "open": parseFloat(originalData.o),
-              "high": parseFloat(originalData.h),
-              "low": parseFloat(originalData.l),
-              "close": parseFloat(originalData.c),
-              "volume": parseFloat(originalData.v),
-              "split": "",
-              "dividend": "",
-              "absoluteChange": "",
-              "percentChange": ""
-            };
+            if (currentTime >= nextCandleStartTime) {
+                // Time to start a new candle
+                console.log("appending new candle");
+                console.log("appending new candle", message);
+                // let originalData = JSON.parse(message);
+                let originalData = JSON.parse(message.server_message);
+                let date = new Date(originalData.t);
+                let newCandle = {
+                    "date": date,
+                    "open": parseFloat(originalData.o),
+                    "high": parseFloat(originalData.h),
+                    "low": parseFloat(originalData.l),
+                    "close": parseFloat(originalData.c),
+                    "volume": parseFloat(originalData.v),
+                    "split": "",
+                    "dividend": "",
+                    "absoluteChange": "",
+                    "percentChange": ""
+                };
 
-            /*const newCandle = {
-                date: nextCandleStartTime,
-                open: ltp,
-                high: ltp,
-                low: ltp,
-                close: ltp,
-                volume: 0 // Adjust the volume as necessary
-            };*/
-            newData.push(newCandle);
-          } else {
-            // Update the last candle with the new LTP
-            // lastCandle.close = ltp;
-            // lastCandle.high = Math.max(lastCandle.high, ltp);
-            // lastCandle.low = Math.min(lastCandle.low, ltp);
-            newData[newData.length - 1] = lastCandle;
-          }
+                /*const newCandle = {
+                    date: nextCandleStartTime,
+                    open: ltp,
+                    high: ltp,
+                    low: ltp,
+                    close: ltp,
+                    volume: 0 // Adjust the volume as necessary
+                };*/
+                newData.push(newCandle);
+            } else {
+                // Update the last candle with the new LTP
+                // lastCandle.close = ltp;
+                // lastCandle.high = Math.max(lastCandle.high, ltp);
+                // lastCandle.low = Math.min(lastCandle.low, ltp);
+                newData[newData.length - 1] = lastCandle;
+            }
 
-          return newData;
+            return newData;
         });
-      };
+    };
 
-      const fetchInitialData = async () => {
+    const fetchInitialData = async () => {
         /*const toDate = new Date();
         toDate.setHours(23, 59, 59, 999);
         const fromDate = new Date();
@@ -224,17 +224,17 @@ function App() {
         console.log("Fetching data", symbol, toDateString);*/
 
         try {
-          // const candleData = await fetchCandleData(symbol, timeFrame, fromDateString, toDateString);
-          // const candleData = await fetchCandleData(symbol, durationData, new Date(new Date().setDate(new Date().getDate() - 151)).toLocaleDateString("sv-SE"), new Date().toLocaleDateString("sv-SE"));
-          const candleData = await fetchCandleData(symbol, 'D', Math.floor(new Date().getTime() / 1000) - (150 * 24 * 3600), Math.floor(new Date().getTime() / 1000));
-          // const candleData = await fetchCandleData(symbol, "d", "2023-08-20", "2024-02-03");
-          console.log(candleData)
+            // const candleData = await fetchCandleData(symbol, timeFrame, fromDateString, toDateString);
+            // const candleData = await fetchCandleData(symbol, durationData, new Date(new Date().setDate(new Date().getDate() - 151)).toLocaleDateString("sv-SE"), new Date().toLocaleDateString("sv-SE"));
+            const candleData = await fetchCandleData(symbol, 'D', Math.floor(new Date().getTime() / 1000) - (150 * 24 * 3600), Math.floor(new Date().getTime() / 1000));
+            // const candleData = await fetchCandleData(symbol, "d", "2023-08-20", "2024-02-03");
+            console.log(candleData)
 
-          setData(candleData)
+            setData(candleData)
         } catch (error) {
-          console.error('Error fetching candle data:', error);
+            console.error('Error fetching candle data:', error);
         }
-      };
+    };
 
 
     const theme = createTheme({
@@ -253,32 +253,33 @@ function App() {
         return <div>Loading...</div>;
     }
 
-  return (
-      <>
-      <ThemeProvider theme={theme}>
-          <CssBaseline />
-      <div className="app-container">
-        <Toolbar style={{
-                borderBottom: 'solid 3px',
-                ...backGroundColor
-            }} />
-        <div className="chart-container">
-          <Sidebar style={{
-              borderRight: 'solid 1px',
-              ...backGroundColor
-          }} />
-          <div className="chart" style={{width: '100%'}}>
-              <StockChart data={data} theme={theme}  height={window.innerHeight-100} ratio={3} width={window.innerWidth-45}/>
-          </div>
-        </div>
-        <Footer style={{
-            borderTop: 'solid 3px',
-            ...backGroundColor
-        }} />
-      </div>
-      </ThemeProvider>
-    </>
-  );
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <div className="app-container">
+                    <Toolbar style={{
+                        borderBottom: 'solid 3px',
+                        ...backGroundColor
+                    }}/>
+                    <div className="chart-container">
+                        <Sidebar style={{
+                            borderRight: 'solid 1px',
+                            ...backGroundColor
+                        }}/>
+                        <div className="chart" style={{width: '100%'}}>
+                            <StockChart data={data} setData={setData} theme={theme} height={window.innerHeight - 100} ratio={3}
+                                        width={window.innerWidth - 45}/>
+                        </div>
+                    </div>
+                    <Footer style={{
+                        borderTop: 'solid 3px',
+                        ...backGroundColor
+                    }}/>
+                </div>
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default App;
