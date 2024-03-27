289cca19e4bb194acb8b59d95bc3e0584224a498
h_sharifi <ujmUJM@1234567>
2024-03-27 13:21:33 +0330

    refactor packages

--- chart-frontend/src/config/constants.ts
+++
@@ -1,8 +0,0 @@
-// export const DATA_ADDRESS = 'http://localhost:5000/fetchCandleData';
-// export const DATA_ADDRESS = 'http://192.168.95.128:5000/fetchCandleData';
-export const DATA_ADDRESS = 'http://127.0.0.1:5000/fetchCandleData';
-// export const DATA_ADDRESS = 'http://192.168.95.128:8080/fetchCandleDataDummy';
-// export const WEBSOCKET_ADDRESS = 'http://localhost:8000';
-// export const WEBSOCKET_ADDRESS = 'ws://192.168.95.128:8004';
-export const WEBSOCKET_ADDRESS = 'ws://127.0.0.1:8000';
-// export const WEBSOCKET_ADDRESS = 'wss://ws.finnhub.io?token=cneoim9r01qq13fns8b0cneoim9r01qq13fns8bg';
---
+++ frontend/src/config/constants.ts
@@ -0,0 +1,8 @@
+// export const DATA_ADDRESS = 'http://localhost:5000/fetchCandleData';
+// export const DATA_ADDRESS = 'http://192.168.95.128:5000/fetchCandleData';
+export const DATA_ADDRESS = 'http://127.0.0.1:5000/fetchCandleData';
+// export const DATA_ADDRESS = 'http://192.168.95.128:8080/fetchCandleDataDummy';
+export const WEBSOCKET_ADDRESS = 'http://localhost:8000';
+// export const WEBSOCKET_ADDRESS = 'ws://192.168.95.128:8000';
+// export const WEBSOCKET_ADDRESS = 'ws://127.0.0.1:8000';
+// export const WEBSOCKET_ADDRESS = 'wss://ws.finnhub.io?token=co1c0lhr01qgulhr2shgco1c0lhr01qgulhr2si0';
