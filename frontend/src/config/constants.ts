79fcb2024e4dace82db1c33d4f9bf2e3afd51a18
mahdi9641 <mahdikhoddam2@gmail.com>
2024-05-16 10:47:43 +0330

    #add add tradingview new

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -2,8 +2,8 @@
 // export const DATA_ADDRESS = 'http://192.168.95.128:5000/fetchCandleData';
 export const DATA_ADDRESS = 'http://127.0.0.1:5000';
 // export const DATA_ADDRESS = 'http://192.168.95.128:8080/fetchCandleDataDummy';
-export const WEBSOCKET_ADDRESS = 'http://127.0.0.1:8000';
-// export const WEBSOCKET_ADDRESS = 'http://91.92.108.4:5555';
+// export const WEBSOCKET_ADDRESS = 'http://127.0.0.1:8000';
+export const WEBSOCKET_ADDRESS = 'http://91.92.108.4:5555';
 // export const WEBSOCKET_ADDRESS = 'http://127.0.0.1:8000';
 // export const WEBSOCKET_ADDRESS = 'ws://192.168.95.128:8000';
 // export const WEBSOCKET_ADDRESS = 'ws://127.0.0.1:8000';
