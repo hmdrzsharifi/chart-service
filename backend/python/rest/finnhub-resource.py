355366802b1518cad8487458663957a948bef072
mahdi9641 <mahdikhoddam2@gmail.com>
2024-05-25 23:51:43 +0330

    #add level 1 cache

--- backend/python/rest/finnhub-resource.py
+++ backend/python/rest/finnhub-resource.py
@@ -1,15 +1,38 @@
+import logging
 import finnhub
 import pandas as pd
 from flask import Flask, request
 from flask_cors import CORS
 import json
+from flask_caching import Cache
+import hashlib
 
 pd.set_option('display.float_format', '{:.8f}'.format)
 
 app = Flask(__name__)
 CORS(app, resources={r'*': {'origins': '*'}})
 
+# Configure caching
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+#redis level 1 caching
+# cache = Cache(app, config={
+# 'CACHE_TYPE': 'redis',
+# 'CACHE_REDIS_URL': 'redis://localhost:6379/0',
+# 'CACHE_DEFAULT_TIMEOUT': 300
+# })
+def make_cache_key():
+    logging.debug("make_cache_key function called")
+    request_data = request.get_json()
+    if request_data:
+        key = json.dumps(request_data, sort_keys=True)  # Sort keys for consistency
+        cache_key = hashlib.md5(key.encode('utf-8')).hexdigest()
+        logging.debug(f"Generated cache key: {cache_key}")
+        return cache_key
+    return None
+
 @app.route('/fetchCandleData', methods=['POST'])
+@cache.cached(timeout=300, key_prefix=make_cache_key)  # Cache for 5 minutes
 def fetch_candle_data():
     request_data = request.json
     symbol = request_data.get('Ticker')
@@ -23,7 +46,7 @@ def fetch_candle_data():
     if time_frame == '30M': time_frame = '30'
     if time_frame == '1H': time_frame = '60'
 
-    finnhub_client = finnhub.Client(api_key="co60qgpr01qmuouob0cgco60qgpr01qmuouob0d0")
+    finnhub_client = finnhub.Client(api_key=SECRET)
     print(request_data)
     # Stock candles
     res = finnhub_client.stock_candles(symbol, time_frame, from_time, to_time)
@@ -35,7 +58,6 @@ def fetch_candle_data():
     else:
         json_data = json.dumps(res)
     return json_data
-
 
 if __name__ == '__main__':
     SECRET = "co60qgpr01qmuouob0cgco60qgpr01qmuouob0d0"
