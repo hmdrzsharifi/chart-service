7519652ec70ab3df64c2fb613f85ba61dede2d35
hmdrz <hmdrzsharifi@gmail.com>
2024-06-21 14:29:09 +0330

    add new api key

--- backend/python/rest/config.py
+++ backend/python/rest/config.py
@@ -1,6 +1,7 @@
 class Config:
     SYMBOLS_API_URL = 'http://91.92.108.4:4444/api/v1/services/all/symbols'
     FINNHUB_API_KEY = 'co60qgpr01qmuouob0cgco60qgpr01qmuouob0d0'
+    FMP_API_KEY = 'fng76oNvOTV9fiNj1QlDCoU1gbZCNtrh'
     HOST = '0.0.0.0'
     PORT = 5000
     DEBUG = False
