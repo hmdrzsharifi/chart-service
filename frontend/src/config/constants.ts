64b49133643e50db0719b19b82cf1907d7b8fa2c
hmdrz <hmdrzsharifi@gmail.com>
2024-06-21 14:28:06 +0330

    refactor

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,8 +1,9 @@
 export const DATA_ADDRESS = 'http://127.0.0.1:5000';
 export const WEBSOCKET_ADDRESS = 'http://91.92.108.4:5555';
+export const SYMBOLS_API_URL = 'http://91.92.108.4:4444/api/v1/services/all/symbols'
 
-
-export const NO_OF_CANDLES = 150;
+export const NO_OF_CANDLES = 300;
+export const LENGTH_TO_SHOW = 300;
 export const STUDIES_CHART_HEIGHT = 150;
 export const TOOLTIP_PADDING_LEFT = 8;
 export const TOOLTIP_PADDING_TOP = 45;
