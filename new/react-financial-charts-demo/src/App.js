f083b22262faefe740e797ae56eca866c5463ef2
hmdrz <hmdrzsharifi@gmail.com>
2024-07-21 00:26:10 +0330

    new project

---
+++ new/react-financial-charts-demo/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import FinancialChart from './FinancialChart';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <h1>React Financial Chart Demo</h1>
+        <FinancialChart />
+      </div>
+  );
+}
+
+export default App;
