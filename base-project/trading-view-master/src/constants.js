297d260c029b7fe1ef457fb58ae9737222ea0f2f
h_sharifi <ujmUJM@1234567>
2024-07-27 17:46:42 +0330

    refactor frontend directory

--- base-project/trading-view-master/src/constants.js
+++
@@ -1,6 +0,0 @@
-export const WEBSOCKET_ADDRESS = 'http://185.148.147.219:5555';
-// export const DATA_ADDRESS = 'http://127.0.0.1:8000';
-// export const PLATFORM_ADDRESS = 'http://185.148.147.219:4444';
-
-export const FMP_DATA_ADDRESS = 'http://127.0.0.1:5000';
-export const FINNHUB_DATA_ADDRESS = 'http://127.0.0.1:8000';
--- frontend/src/type/TrendLineType.ts
+++
@@ -1,7 +0,0 @@
-export type TrendLineType = {
-    start: [],
-    end: [],
-    appearance: any,
-    type: string,
-    selected: boolean
-}
